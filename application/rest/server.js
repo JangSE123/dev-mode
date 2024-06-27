657ceaac4a30cf809a953c8c3ccbe677a4b40489
JHL222 <jmejjh2@gmail.com>
2024-06-27 15:20:00 +0900

    payment, musicregister

--- application/rest/server.js
+++ application/rest/server.js
@@ -6,7 +6,7 @@ let sdk = require('./sdk');
 const PORT = 8001;
 const HOST = '0.0.0.0';
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/init', function (req, res) {
    let user = req.query.user;
@@ -30,7 +30,15 @@ app.get('/payment', function (req, res) {
    let amount = req.query.amount;
    let args = [buyer, seller, amount];
    sdk.send(false, 'payment', args, res);
-})
+});
+
+app.get('/musicregister', function (req, res) {
+   let seller = req.query.seller;
+   let music = req.query.music;
+   let price = req.query.price;
+   let args = [seller, music, price];
+   sdk.send(false, 'musicRegister', args, res);
+});
 
 app.get('/query', async function (req, res) {
    try {
@@ -38,7 +46,7 @@ app.get('/query', async function (req, res) {
       let cashArgs = [name + '_cash'];
       let pointArgs = [name + '_point'];
 
-      let cashResult = await sdk.query('query',cashArgs);
+      let cashResult = await sdk.query('query', cashArgs);
       let pointResult = await sdk.query('query', pointArgs);
 
       res.json({
@@ -46,18 +54,9 @@ app.get('/query', async function (req, res) {
          point: pointResult.toString()
       });
    } catch (error) {
-      console.log(`Failed to Query : ${error}`);
       res.status(500).send(error.toString());
    }
 });
-
-app.get('/delete', function(req, res) {
-   let name = req.query.name;
-   let args = [name];
-   sdk.send(false, 'delete', args, res);
-})
-
-
 
 app.use(express.static(path.join(__dirname, '../client')));
 app.listen(PORT, HOST);
